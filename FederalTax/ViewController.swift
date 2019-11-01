2f7c72524a761252b3c6ce1b5d5adcb8ea371249
Keira Hartley <khartley@lcs.on.ca>
2019-11-01 10:05:18 -0400

    Completed view.

--- FederalTax/ViewController.swift
+++ FederalTax/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK: Properties
+    
+    @IBOutlet weak var inputName: UITextField!
+    
+    @IBOutlet weak var annualIncome: UITextField!
+    
+    //MARK: Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
+    @IBAction func calculateTax(_ sender: Any) {
+        
+    }
+    
+    
 
 }
 
